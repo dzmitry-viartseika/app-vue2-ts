90dd9e9c19e0d4a37a07fc5172ff5b6b69b28045
Dmitry Verteyko <verteyko1990@gmail.com>
2021-05-04 22:26:45 +0300

    added ts request

--- src/api/usersApi/api.ts
+++ src/api/usersApi/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { UserSubmit } from '@/store/models';
+import { UserSubmit } from '@/store/models.d';
 import { CURRENT_SERVER } from '@/api/domain';
 
 export default {
